0c5d43be76afafc2dd2f6cbcba9ad1d1790d28ac
Atlas <agwny94@gmail.com>
2013-03-14 11:21:09 -0400

    Added movement buttons, fixed rocks

--- BlackScorpion/Assets/Resources/Scripts/ButtonMovement.cs
+++ BlackScorpion/Assets/Resources/Scripts/ButtonMovement.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
-public class ButtonToggleBlockTransparency : MonoBehaviour {
-	
+public class ButtonMovement : MonoBehaviour {
+
 	public GameObject[] objsToToggle;
 	public bool persistent = false;
 	public float toggleTime = 1.5f;
@@ -10,14 +10,14 @@ public class ButtonToggleBlockTransparency : MonoBehaviour {
 	void OnTriggerEnter(Collider collision) {
 		if(collision.gameObject.tag == "Player"){ 
 			animation.Play("down");
-			foreach(GameObject obj in objsToToggle) { obj.GetComponent<TransparencyHandler>().makeSolid(); }
+			foreach(GameObject obj in objsToToggle) { obj.GetComponent<PingPongCube>().start(); }
 			StartCoroutine("pressCountdown");
 		}
 	}
 	
 	private IEnumerator pressCountdown() {
 		yield return new WaitForSeconds(toggleTime);
-		if(!persistent) foreach(GameObject obj in objsToToggle) { obj.GetComponent<TransparencyHandler>().makeTransparent(); }
+		if(!persistent) foreach(GameObject obj in objsToToggle) { obj.GetComponent<PingPongCube>().stop(); }
 		animation.Play("up");
 	}
 }
--- BlackScorpion/Assets/Resources/Scripts/PingPongCube.cs
+++ BlackScorpion/Assets/Resources/Scripts/PingPongCube.cs
@@ -5,14 +5,30 @@ public class PingPongCube : MonoBehaviour {
 	
 	public GameObject endPos;
 	public float time, delay;
+	public bool autoRun = true;
 	
 	private bool running = false;
+	private float id;
+	
+	void Start() {
+		id = Random.value * Random.value;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(!running) {
-			running = true;
-			iTween.MoveTo(gameObject, iTween.Hash("time", time, "position", endPos.transform.position, "easeType", "linear", "loopType", "pingPong", "delay", delay));
+		if(autoRun) {
+			if(!running) {
+				running = true;
+				iTween.MoveTo(gameObject, iTween.Hash("name", id.ToString(), "time", time, "position", endPos.transform.position, "easeType", "linear", "loopType", "pingPong", "delay", delay));
+			}
 		}
+	}
+	
+	public void start() {
+		iTween.MoveTo(gameObject, iTween.Hash("name", id.ToString(), "time", time, "position", endPos.transform.position, "easeType", "linear", "loopType", "pingPong", "delay", delay));
+	}
+	
+	public void stop() {
+		iTween.StopByName(id.ToString());
 	}
 }
